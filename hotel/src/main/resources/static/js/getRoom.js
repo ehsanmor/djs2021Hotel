f22eec968d9ebce1de47f9250f27c861667ad156
Yonas <yoni11er@gmail.com>
2021-07-11 19:08:22 +0200

    show guests and edit guest ui ready

--- hotel/src/main/resources/static/js/getRoom.js
+++ hotel/src/main/resources/static/js/getRoom.js
@@ -1,57 +1,54 @@
-$(function () {
+$(document).ready(function () {
+    var url = new URL(window.location.href);
+    var id = url.searchParams.get("id");
+    if (id !== null) getRoomById(id);
+});
 
-    $(document).ready(function () {
-        var url = new URL(window.location.href);
-        var id = url.searchParams.get("id");
-        if (id !== null) getRoomById(id);
-    });
-
-    function getRoomById(id) {
-        $.ajax({
-            url: 'http://localhost:8080/hotel/room/' + id,
-            type: 'GET',
-        }).done(function (data) {
+function getRoomById(id) {
+    $.ajax({
+        url: 'http://localhost:8080/hotel/room/' + id,
+        type: 'GET',
+    }).done(function (data) {
 //            console.log(data.roomType);
-            $('#roomId').val(data.id);
-            var selectedRoomType = data.type.toLowerCase();
-            $('input:radio[name="roomType"]').filter('[value="'+ selectedRoomType +'"]').attr('checked', true);
+        $('#roomId').val(data.id);
+        var selectedRoomType = data.type.toLowerCase();
+        $('input:radio[name="roomType"]').filter('[value="'+ selectedRoomType +'"]').attr('checked', true);
 //            $('#roomType').val(data.roomType);
-            $('#roomSize').val(data.capacity);
-            $('#roomPrice').val(data.price);
-            $('#roomLastBigClean').val(data.lastBigCleaningDate);
-            $('#roomBigCleanAfterInDays').val(data.numberOfDaysAfterBigClean);
-            var selId = document.getElementById("roomUnderConstruction");
-            selId.value = data.underConstruction?"yes":"no";
+        $('#roomSize').val(data.capacity);
+        $('#roomPrice').val(data.price);
+        $('#roomLastBigClean').val(data.lastBigCleaningDate);
+        $('#roomBigCleanAfterInDays').val(data.numberOfDaysAfterBigClean);
+        var selId = document.getElementById("roomUnderConstruction");
+        selId.value = data.underConstruction?"yes":"no";
 //            $('#roomUnderConstruction').value = data.roomUnderConstruction?1:2;
 
-            showFacilities(data.facilities.toString());
-        });
-    }
+        showFacilities(data.facilities.toString());
+    });
+}
 
-    // click on button submit
-    $("#submit").on('click', function () {
-        getFacilities();
-        $.ajax({
-            url: 'http://localhost:8080/hotel/room/add',
-            type: 'PUT',
-            contentType: 'application/json',
-            dataType: "json",
-            data: JSON.stringify({
+// click on button submit
+$("#submit").on('click', function () {
+    getFacilities();
+    $.ajax({
+        url: 'http://localhost:8080/hotel/room/add',
+        type: 'PUT',
+        contentType: 'application/json',
+        dataType: "json",
+        data: JSON.stringify({
 
-                "roomId": document.getElementById('roomId').value,
-                "roomType": document.getElementById('roomType').value,
-                "roomSize": document.getElementById('roomSize').value,
-                "roomPrice": document.getElementById('roomPrice').value,
-                "roomFacilities": document.getElementById('roomFacilities').value
-            }),
-            success: function (result) {
-                $("#messageLabel").html("Your data was successfully saved.");
-                setTimeout("$('#messageLabel').html('');", 2000);
-                setTimeout("location.href = 'http://localhost:8080/rooms.html';", 2000);
-            },
-            error: function (e) {
-                console.log(e);
-            },
-        });
+            "roomId": document.getElementById('roomId').value,
+            "roomType": document.getElementById('roomType').value,
+            "roomSize": document.getElementById('roomSize').value,
+            "roomPrice": document.getElementById('roomPrice').value,
+            "roomFacilities": document.getElementById('roomFacilities').value
+        }),
+        success: function (result) {
+            $("#messageLabel").html("Your data was successfully saved.");
+            setTimeout("$('#messageLabel').html('');", 2000);
+            setTimeout("location.href = 'http://localhost:8080/rooms.html';", 2000);
+        },
+        error: function (e) {
+            console.log(e);
+        },
     });
 });
